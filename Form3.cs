3ea84127ea07d1f2afd960dcbc6c9742d812e556
pluxury golf <pluxury golf@DESKTOP-SV9M877>
2021-05-21 16:39:21 +0300

    Добавьте файлы проекта.

---
+++ Form3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Menu
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ActiveForm.Hide();
+            Form1 frm1 = new Form1();
+            frm1.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
